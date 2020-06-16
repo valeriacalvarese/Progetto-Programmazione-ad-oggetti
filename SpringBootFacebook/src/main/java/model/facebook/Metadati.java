b9ef71b78d23a28a5062ba75447e6125f52fa0b2
valeriacalvarese <64692021+valeriacalvarese@users.noreply.github.com>
2020-06-16 21:30:35 +0200

    add Metadati.class

---
+++ SpringBootFacebook/src/main/java/model/facebook/Metadati.java
@@ -0,0 +1,47 @@
+package model.facebook;
+
+public class Metadati 
+{ 
+	private String name; //nome campo
+	private String type; //tipo campo
+	private String property; //descrizione campo
+	
+	public Metadati(String name, String type, String property)
+	{
+		this.name = name;
+		this.type =type;
+		this.property= property;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getProperty() {
+		return property;
+	}
+
+	public void setProperty(String property) {
+		this.property = property;
+	}
+
+	@Override
+	public String toString() {
+		return this.getName()+ ":"+this.getProperty()+"\n"+this.getType();
+	}
+	
+	
+
+}
