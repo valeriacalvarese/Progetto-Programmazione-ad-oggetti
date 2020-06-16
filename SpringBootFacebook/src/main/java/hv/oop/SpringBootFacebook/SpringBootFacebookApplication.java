d21421db4495d43c8e560a343215e76f548027bf
valeriacalvarese <64692021+valeriacalvarese@users.noreply.github.com>
2020-06-16 20:08:24 +0200

    created main

--- SpringBootFacebook/src/main/java/hv/oop/SpringBootFacebook/SpringBootFacebookApplication.java
+++ SpringBootFacebook/src/main/java/hv/oop/SpringBootFacebook/SpringBootFacebookApplication.java
@@ -1,13 +1,47 @@
 package hv.oop.SpringBootFacebook;
 
+/*
+* 
+*  
+* @author Valeria Calvarese
+* @author Hermes Karalliu
+*/
+import java.io.IOException;
+
+import java.net.MalformedURLException;
+
+import org.json.JSONException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import util.facebook.Storage;
+
 @SpringBootApplication
-public class SpringBootFacebookApplication {
-
-	public static void main(String[] args) {
+public class SpringBootFacebookApplication
+{
+	public static void main(String[] args) throws MalformedURLException, IOException, JSONException
+	{
+		try
+		{
+		Storage.fill_post(
+				"https://graph.facebook.com/v7.0/PerriSristorantePizzeria/feed?access_token=EAAEBaCJy5BwBAIruMFDGMl91xciu9nbcXZCEl4o6hxWgiV0IW2gZAVcFZCVtZC7Wzz31z5WVZCJoEdX0OeLHU5NZBc2dZAL0SaliVY2jkq9ioI5PQ6rFxA61fGssIN3HP2WMkKudehNctbQYY8ql1Lw7cr07CjE3JToP9kH53BjXAZDZD");
+		}
+	catch (MalformedURLException e)
+		{
+		e.printStackTrace();
+		}
+		
+	catch (IOException e)
+		{
+		e.printStackTrace();
+		}
+		
+	catch (JSONException e)
+		{
+		e.printStackTrace();
+		}
+		
 		SpringApplication.run(SpringBootFacebookApplication.class, args);
-	}
-
+	
+}
 }
