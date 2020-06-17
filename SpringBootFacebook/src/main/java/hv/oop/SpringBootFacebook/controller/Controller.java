190953451527d448e903daa0f88a8ff1c09a9475
valeriacalvarese <64692021+valeriacalvarese@users.noreply.github.com>
2020-06-17 14:13:51 +0200

    correzione Controller

--- SpringBootFacebook/src/main/java/hv/oop/SpringBootFacebook/controller/Controller.java
+++ SpringBootFacebook/src/main/java/hv/oop/SpringBootFacebook/controller/Controller.java
@@ -13,14 +13,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import model.facebook.Metadati;
 import model.facebook.Post;
+import model.facebook.Foto;
 import util.facebook.Storage;
-
+import org.springframework.web.bind.annotation.RestController;
 
 /* 
 *  
 * @author Valeria Calvarese
 * @author Hermes Karalliu
 */
+@RestController
 public class Controller 
 {
 	@GetMapping("/post")
@@ -34,6 +36,13 @@ public class Controller
 	{
 		return Storage.fill_Metadati();
 	}
-
+	@GetMapping("/foto")
+	public ArrayList<Foto> getArrayFoto()
+	{
+		return Storage.get_Foto();
+	}
+	
+	
+	
 
 }
