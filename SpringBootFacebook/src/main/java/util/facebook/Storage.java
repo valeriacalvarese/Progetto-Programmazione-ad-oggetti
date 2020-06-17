abcf15df1b1f299cd81b311f65fe2547eeef3e3f
valeriacalvarese <64692021+valeriacalvarese@users.noreply.github.com>
2020-06-17 14:15:39 +0200

    add Metadati

--- SpringBootFacebook/src/main/java/util/facebook/Storage.java
+++ SpringBootFacebook/src/main/java/util/facebook/Storage.java
@@ -14,6 +14,7 @@ import org.json.JSONException;
 
 import model.facebook.Metadati;
 import model.facebook.Post;
+import model.facebook.Foto;
 
 
 public class Storage
@@ -21,6 +22,7 @@ public class Storage
 	
 	public static ArrayList<Metadati> metadati = new ArrayList<Metadati>();
 	public static ArrayList<Post> post = new ArrayList<Post>();
+	public static ArrayList<Foto> foto = new ArrayList<Foto>();
 	
 	public static void fill_post(String string1) throws MalformedURLException, IOException, JSONException
 	{
@@ -44,12 +46,19 @@ public class Storage
 		} 
 		
 	}
-
+	
+	
 	public static ArrayList<Metadati> fill_Metadati() {
 		// TODO Auto-generated method stub
 		metadati.add(new Metadati("id","String","identificator"));
 		metadati.add(new Metadati("message","String","description"));
 		metadati.add(new Metadati("create_time","String","whenwaspublished"));
+		metadati.add(new Metadati("story","String","richiamo"));
+		metadati.add(new Metadati("size","ArrayList","dimensioni relativa all'immagine"));
+		metadati.add(new Metadati("height","int","altezza relativa all'immagine"));
+		metadati.add(new Metadati("width","int","larghezza relativa all'immagine"));
+		metadati.add(new Metadati("resize","String","ridimensioni immagine"));
+		
 		return metadati;
 	}
 
@@ -57,4 +66,10 @@ public class Storage
 		// TODO Auto-generated method stub
 		return post;
 	}
+	public static ArrayList<Foto> get_Foto(){
+		
+			return foto;
+		
+	}
+	
 }
