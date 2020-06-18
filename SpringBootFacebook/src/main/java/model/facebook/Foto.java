7e767486e1007d6c375339d1b22b2a8b18af0aa0
valeriacalvarese <64692021+valeriacalvarese@users.noreply.github.com>
2020-06-18 22:05:54 +0200

    modifiche

--- SpringBootFacebook/src/main/java/model/facebook/Foto.java
+++ SpringBootFacebook/src/main/java/model/facebook/Foto.java
@@ -1,36 +1,58 @@
 package model.facebook;
-
 /** Classe Foto che estende la classe post;
  *  
 * @author Valeria Calvarese
-* @author Hermes Karalliu
+* 
 */
- 
-public class Foto extends Post {
+public class Foto extends Post{
 	
-	private String id_foto =null;
-	private String media_url = null;
-	private int pixelWide=0; //larghezza 
-	private int pixelHeight=0; //altezza 
-	private int nbyte=0;
-	private String resize= null; //adattamento delle immagini
 	
-	public Foto (String id_foto, String media_url, String resize)
+	private String id;
+	private String media_url ;
+	private int pixelWidth; //larghezza 
+	private int pixelHeight; //altezza 
+	private int nbyte;
+	private String full_picture;
+
+	
+	public Foto ()
 	{
 		super();
-		this.id_foto = id_foto;
-		this.media_url =media_url;
-		this.resize = resize;
-		
+		this.id = null;
+		this.media_url =null;
+		this.pixelHeight = 0;
+		this.pixelWidth = 0;
+		this.nbyte =0 ;
+		this.full_picture = null;
+			
 	}
 	
-
-	public String getId_foto() {
-		return id_foto;
+	public Foto(String id, String media_url, String full_picture)
+	{
+		this.id =id;
+		this.media_url=media_url;
+		this.full_picture=full_picture;
+		
+	}
+	public boolean getFoto() 
+	{
+		if(this.media_url != null)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		
+		}
+		}
+	
+		public String getId() {
+		return id;
 	}
 
-	public void setId_foto(String id_foto) {
-		this.id_foto = id_foto;
+	public void setId(String id) {
+		this.id = id;
 	}
 
 	public String getMedia_url() {
@@ -41,12 +63,12 @@ public class Foto extends Post {
 		this.media_url = media_url;
 	}
 
-	public int getPixelWide() {
-		return pixelWide;
+	public int getPixelWidth() {
+		return pixelWidth;
 	}
 
-	public void setPixelWide(int pixelWide) {
-		this.pixelWide = pixelWide;
+	public void setPixelWidth(int pixelWidth) {
+		this.pixelWidth = pixelWidth;
 	}
 
 	public int getPixelHeight() {
@@ -65,18 +87,25 @@ public class Foto extends Post {
 		this.nbyte = nbyte;
 	}
 	
-	public String getResize() {
-		return resize;
+	
+	
+	
+	
+
+	public String getFull_picture() {
+		return full_picture;
 	}
 
-	public void setResize(String resize) {
-		this.resize = resize;
+	public void setFull_picture(String full_picture) {
+		this.full_picture = full_picture;
 	}
 
 	@Override
 	public String toString() {
-		return "Foto [id_foto=" + id_foto + ", media_url=" + media_url + ", pixelWide=" + pixelWide + ", pixelHeight="
-				+ pixelHeight + ", nbyte=" + nbyte + ",resize=" + resize +"]";
+		return "Foto [id=" + id + ", media_url=" + media_url + ", pixelWidth=" + pixelWidth + ", pixelHeight="
+				+ pixelHeight + ", nbyte=" + nbyte + ", full_picture=" + full_picture + "]";
+	
+	
 	}
 	
 } 
