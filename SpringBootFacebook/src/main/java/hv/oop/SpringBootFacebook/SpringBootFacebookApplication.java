a4b924539a812bcfcfb90156b10e206b85f032a2
valeriacalvarese <64692021+valeriacalvarese@users.noreply.github.com>
2020-06-23 19:30:51 +0200

    add Storage.fill_foto(URL) and Parsing.containPhoto

--- SpringBootFacebook/src/main/java/hv/oop/SpringBootFacebook/SpringBootFacebookApplication.java
+++ SpringBootFacebook/src/main/java/hv/oop/SpringBootFacebook/SpringBootFacebookApplication.java
@@ -14,7 +14,7 @@ import org.json.JSONException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import util.facebook.Storage;
+import util.facebook.*;
 
 @SpringBootApplication
 public class SpringBootFacebookApplication
@@ -25,7 +25,8 @@ public class SpringBootFacebookApplication
 		{
 		Storage.fill_post(
 				"https://graph.facebook.com/v7.0/PerriSristorantePizzeria/feed?access_token=EAAEBaCJy5BwBAIruMFDGMl91xciu9nbcXZCEl4o6hxWgiV0IW2gZAVcFZCVtZC7Wzz31z5WVZCJoEdX0OeLHU5NZBc2dZAL0SaliVY2jkq9ioI5PQ6rFxA61fGssIN3HP2WMkKudehNctbQYY8ql1Lw7cr07CjE3JToP9kH53BjXAZDZD");
-		
+		Storage.fill_foto("https://graph.facebook.com/v7.0/PerriSristorantePizzeria/feed?fields=full_picture&access_token=EAAEBaCJy5BwBAIruMFDGMl91xciu9nbcXZCEl4o6hxWgiV0IW2gZAVcFZCVtZC7Wzz31z5WVZCJoEdX0OeLHU5NZBc2dZAL0SaliVY2jkq9ioI5PQ6rFxA61fGssIN3HP2WMkKudehNctbQYY8ql1Lw7cr07CjE3JToP9kH53BjXAZDZD");
+		Parsing.ContainPhoto(Storage.post,Storage.foto);
 		
 			}
 			catch (MalformedURLException e)
