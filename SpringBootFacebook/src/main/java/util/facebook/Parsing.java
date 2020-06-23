00e0c5a60d14f638070d81e11162fdf35c4cb3a7
valeriacalvarese <64692021+valeriacalvarese@users.noreply.github.com>
2020-06-23 17:05:00 +0200

    add setContainPhoto()

--- SpringBootFacebook/src/main/java/util/facebook/Parsing.java
+++ SpringBootFacebook/src/main/java/util/facebook/Parsing.java
@@ -9,23 +9,29 @@ package util.facebook;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
-import model.facebook.Post;
-import model.facebook.Foto;
+import model.facebook.*;
+import util.facebook.*;
+import hv.oop.SpringBootFacebook.controller.*;
 
-import util.facebook.Parsing;
 
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Parsing {
 	 
-	 public static ArrayList<Post> getPost(String link) throws MalformedURLException, IOException, JSONException
+
+
+	private static ArrayList<Post> post;
+	public static ArrayList<Post> getPost(String link) throws MalformedURLException, IOException, JSONException
 	 	 {
 			 String url =null;
 			 ArrayList<Post> post = new ArrayList<Post>();
@@ -67,40 +73,65 @@ public class Parsing {
 			 }
 			 return post;
 	 	 }
-			 public static ArrayList<Foto> getFoto(String link) throws MalformedURLException, IOException, JSONException
-		 	 {
-				 String url =null;
-				 ArrayList<Foto> foto = new ArrayList<Foto>();
-				 //Controllo eccezioni con try-catch
-				 try {
-					 url = Reading.read_url (link);
-				  }
-				 catch (MalformedURLException e) {
-					 e.printStackTrace();
-				 }
-				 catch (IOException e) {
-					 e.printStackTrace();
-				 }
-				 
-				 
-				  JSONObject object =null;
-				 try {
-					 object =new JSONObject(url);
-				 }
-				 catch (JSONException e){
-					 e.printStackTrace ();
-				 }
-				 
-				 JSONArray array = object.getJSONArray("data");
-				 for (int i=0; i<array.length(); i++)
-				 {
-					 JSONObject o4 = object.getJSONArray("data").getJSONObject(i);
-					 ObjectMapper o5 =new ObjectMapper ();
-					 Foto o6  = o5.readValue(o4.toString(), Foto.class);
-					 foto.add(o6);
-				 }
-				 
-				 
-			 return foto; }
-	}
+	
+	public static ArrayList<Post> getFoto(String link) throws JSONException, IOException
+	{
+		 String url =null;
+		 ArrayList<Post> foto = new ArrayList<Post>();
+		 //Controllo eccezioni con try-catch 
+		 try 
+		 {
+			 URL url1 = new URL("https://graph.facebook.com/v7.0/PerriSristorantePizzeria/feed?fields=full_picture&access_token=EAAEBaCJy5BwBAIruMFDGMl91xciu9nbcXZCEl4o6hxWgiV0IW2gZAVcFZCVtZC7Wzz31z5WVZCJoEdX0OeLHU5NZBc2dZAL0SaliVY2jkq9ioI5PQ6rFxA61fGssIN3HP2WMkKudehNctbQYY8ql1Lw7cr07CjE3JToP9kH53BjXAZDZD");
+			 
+		 }
+		 catch (MalformedURLException e)
+		 {
+			 e.printStackTrace();
+		 }
+		 catch (IOException e)
+		 {
+			 e.printStackTrace();
+		 } 
+		 JSONObject o =null;
+		 try {
+			 o =new JSONObject(url);
+		 }
+		 catch (JSONException e){
+			 e.printStackTrace ();
+		 }
+		 
+		 JSONArray array2 = o.getJSONArray("data");
+		 
+		 for (int i=0; i<array2.length(); i++)
+		 {
+			 JSONObject o4 = o.getJSONArray("data").getJSONObject(i);
+			 ObjectMapper o5 =new ObjectMapper ();
+			 Post o6  = o5.readValue(o4.toString(), Post.class);
+			 foto.add(o6);
+					 
+		 }
+		 return foto;
+	 }
 
+	static boolean ContainPhoto()
+	{
+	int j=0; 
+		//array che contine i link delle foto
+		ArrayList<Post> a;
+		
+		for(int i=0;i<a.size();i++)
+		{ 
+
+			while (a.get(i).getId.compareTo(post.get(j).getId)!=0 || j<post.size()) 
+			{
+				j++; 
+				if(j<post.size()) 
+					{
+						  post.get(j).setContainPhoto(true); 
+					}
+			}
+		}
+	}  
+}
+
+			
