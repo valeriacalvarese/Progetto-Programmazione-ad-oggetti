c6d4a02bf86724a7549282880b0234c9a05919f4
valeriacalvarese <64692021+valeriacalvarese@users.noreply.github.com>
2020-06-28 17:11:45 +0200

    Comments Javadoc

--- SpringBootFacebook/src/main/java/hv/oop/SpringBootFacebook/SpringBootFacebookApplication.java
+++ SpringBootFacebook/src/main/java/hv/oop/SpringBootFacebook/SpringBootFacebookApplication.java
@@ -1,6 +1,6 @@
 package hv.oop.SpringBootFacebook;
 
-/*
+/**
 * 
 *  
 * @author Valeria Calvarese
@@ -24,6 +24,8 @@ public class SpringBootFacebookApplication
 		try
 		{
 			//link post
+			  
+			
 		Storage.fill_post(
 				"https://graph.facebook.com/v7.0/PerriSristorantePizzeria/feed?access_token=EAAEBaCJy5BwBAIruMFDGMl91xciu9nbcXZCEl4o6hxWgiV0IW2gZAVcFZCVtZC7Wzz31z5WVZCJoEdX0OeLHU5NZBc2dZAL0SaliVY2jkq9ioI5PQ6rFxA61fGssIN3HP2WMkKudehNctbQYY8ql1Lw7cr07CjE3JToP9kH53BjXAZDZD");
 			//link foto
